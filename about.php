c9e8e9307c399186d5748f5fbe5f26ca6137a427
Jazzest <josephwhacheng98@gmail.com>
2020-06-10 16:19:00 -0600

    first commit

---
+++ about.php
@@ -0,0 +1,24 @@
+<?php include_once 'includes/templates/header.php'; ?>
+
+  <!-- Hero -->
+  <header class="hero hero-2 text-white text-center">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <h1>Sobre PetClub</h1>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Description -->
+  <section class="description text-center">
+    <div class="container">
+      <p><b>PetClub</b> es una empresa para perros y gatos, ubicado en Lima, enfocado en el cuidado de alta calidad para sus mascotas. Nosotros nacemos por tener un profundo amor por los animales, con el objetivo de atender a nuestros clientes según las necesidades de cada uno y hacerlos vivir la mejor experiencia posible.</p>
+      <p>Sabemos que cada mascota es distinta, por lo que evaluamos el carácter de cada nuevo cliente para poder atenderlo como se merece y convertir los días que está lejos de su dueño en unas vacaciones inolvidables. Lo más importante para nosotros es tu confianza y tranquilidad</p>
+      <p><b>¡Somos conscientes de lo difícil que es dejar a nuestros hijos al cuidado de otro!</b></p>
+    </div>
+  </section>
+
+<?php include_once 'includes/templates/footer.php'; ?>
